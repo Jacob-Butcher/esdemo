47ad8318c487dc1c0497023c525eca4a6bf0f495
geigeiguo <a1091561148@qq.com>
2026-04-13 22:54:50 +0800

    init

---
+++ app/domain/src/main/java/com/example/esDemo/model/Product.java
@@ -0,0 +1,43 @@
+package com.example.esDemo.model;
+
+import java.util.Date;
+
+public class Product {
+    private String name;
+    private double price;
+    private Date date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Product(){
+
+    }
+
+    public Product(String name, double price, Date date) {
+        this.name = name;
+        this.price = price;
+        this.date = date;
+    }
+}
